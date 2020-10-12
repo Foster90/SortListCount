3dab424082cd5fee5e2c31378afd11f44ce5ec53
Matthew Foster <Matthew.Foster@Civica.co.uk>
2020-10-12 20:04:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var model = new Model();
+            model.Items.Add(new Item { Name = "item1" });
+            model.Items.Add(new Item { Name = "item1" });
+            model.Items.Add(new Item { Name = "item1" });
+            model.Items.Add(new Item { Name = "item2" });
+            model.Items.Add(new Item { Name = "item2" });
+            model.Items.Add(new Item { Name = "item3" });
+
+            foreach (var entry in model.ItemNameAndCount)
+            {
+                Console.WriteLine(entry.name + ": " + entry.count);
+            }
+        }
+
+        public class Model
+        {
+            public List<Item> Items { get; set; } = new List<Item>();
+
+            public IEnumerable<(string name, int count)> ItemNameAndCount
+               => Items.GroupBy(x => x.Name)
+                       .Select(group =>
+                           (
+                             name: group.Key,
+                             count: group.Count()
+                           )
+                        );
+        }
+
+        public class Item
+        {
+            public string Name { get; set; }
+        }
+    }
+}
